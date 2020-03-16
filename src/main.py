723cce17566e0eee33d936917496efff40d0c371
Jud1cator <yarik200027@gmail.com>
2020-03-16 17:05:51 +0300

    Fix button style, add JSON export

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, request
+import json
+
 
 app = Flask(__name__)
-default_css = open("stylesheets/style.css", "r").read()
+default_css = open(".\\stylesheets\\style.css", "r").read()
 
 @app.route('/')
 def url_index():
-	buttons = {
-		"Create tutorial" : "/create_tutorial",
-	}
-	return render_template('index.html', buttons = buttons, default_css = default_css)
+    buttons = {
+        "Create tutorial" : "/create_tutorial",
+    }
+    return render_template('index.html', buttons = buttons, default_css = default_css)
 
 @app.route('/create_tutorial', methods = ["POST", "GET"])
 def url_create_tutorial():
-	if request.method == "GET":
-		return render_template("create_tutorial.html", default_css=default_css)
+    if request.method == "GET":
+        return render_template("create_tutorial.html", default_css=default_css)
 
+    text = request.form["text"]
+    presentation = {"text": text}
+    with open('presentation.json', 'w') as outfile:
+        json.dump(presentation, outfile)
+    return render_template("create_tutorial.html", default_css=default_css)
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
