0d28f0b4fa41b79f8262c2db82766f4119282b30
dearrdeer <baickov@gmail.com>
2020-03-31 20:55:56 +0300

    pushed src

---
+++ web-client/src/views/Create.vue
@@ -0,0 +1,14 @@
+<template>
+<v-image-input v-model="imageData" :image-quality="0.85" clearable image-format="jpeg" />
+</template>
+
+<script>
+import VImageInput from 'vuetify-image-input';
+
+export default {
+  name: 'Create',
+  components: {
+    'v-image-input': VImageInput,
+  },
+};
+</script>
