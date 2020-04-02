36740a6bac97ec59aa66065d970f5b054f756b05
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-04-02 22:02:13 +0300

    [Enhancements] Add gltf exporter scripts

---
+++ gltf exporter/test_pygltf.py
@@ -0,0 +1,11 @@
+from pygltflib import GLTF2
+from pygltflib.utils import ImageFormat, Image
+gltf = GLTF2().load("box.gltf")
+image = Image()
+image.uri = "sample.png"
+gltf.images.append(image)
+gltf.convert_images("png")
+gltf.images[0].uri  # will now be something like "data:image/png;base64,iVBORw0KGg..."
+gltf.images[0].name  # will be myfile.png
+
+gltf.save("sample.gltf")
