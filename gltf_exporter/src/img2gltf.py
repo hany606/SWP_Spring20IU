764ebd6d34df5396f68c4ce9bad29823bce5a7f6
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-04-03 21:45:35 +0300

    [Enhancements] Make the gltf rectangle instead of plane for better visualization

--- gltf_exporter/src/img2gltf.py
+++ gltf_exporter/src/img2gltf.py
@@ -3,20 +3,20 @@ import shutil
 import json
 
 
-def create_slide(uri):
-    shutil.copyfile("Plane.bin", uri+".bin")
-    with open("Plane.gltf","r") as read_file:
+def create_slide(out, uri):
+    shutil.copyfile("Box.bin", uri+".bin")
+    with open("Box.gltf","r") as read_file:
         data = json.load(read_file)
         data["nodes"][0]["name"] = uri
         data["buffers"][0]["uri"] = uri+".bin"
         data["images"][0]["uri"] = uri+".png"
         data["materials"][0]["name"] = uri
         data["meshes"][0]["name"] = uri
-        with open(uri+".gltf","w+") as write_file:
+        with open(out+uri+".gltf","w+") as write_file:
             json.dump(data, write_file)
 
 out_path = "../tutorial1/"
 
-create_slide(out_path+"SlideSample")
+create_slide(out_path, "SlideSample")
 gltf = GLTF.load(out_path+'SlideSample.gltf')
 gltf.export(out_path+'SlideSample.gltf')
