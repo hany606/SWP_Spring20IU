e6942bb26d845716e8efe790605b968f4385175f
Ayaz <baickov@gmail.com>
2020-04-03 13:37:05 +0300

    Made separate branch for front

---
+++ web-client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-toolbar-title>Virtual Assistant<br> </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn text small href="./">Home</v-btn>
+      <newPres></newPres>
+    </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Create from '@/views/Create.vue';
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+  components: {
+    newPres: Create,
+  },
+};
+</script>
---
+++ web-client/src/views/Create.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn text small v-on:click="onClick()">create</v-btn>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Create',
+  methods: {
+    onClick() {
+      const path = 'http://localhost:5000/create';
+      axios.post(path);
+    },
+  },
+};
+
+</script>
