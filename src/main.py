de5ad2b22100e893646507a2ff10896909f1fbde
Jud1cator <yarik200027@gmail.com>
2020-04-04 17:48:22 +0300

    Test connection to DB from backend

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from PIL import Image
+from pymongo import MongoClient
 import json
 import uuid
 import base64
 
 
-app = Flask(__name__)
+dbclient = None
 
+app = Flask(__name__)
 CORS(app)
 
-presentations = []
-
-new_id = '' 
 #images are base64 encoded!
 
-@app.route('/', methods = ["GET"])
-def url_explore():
-    return jsonify(presentations)
+# @app.route('/', methods = ["GET"])
+# def url_explore():
+#     return jsonify(presentations)
 
 @app.route('/create', methods = ["POST"])
 def url_create(): 
@@ -28,27 +26,33 @@ def url_create():
             'title': 'Untitled',
             'slides' : []
         }
-        presentations.append(new_presentation)
         response_object['id'] = new_presentation['id']
+        dbclient.presentations.insert_one(new_presentation)
+        print("Presentation is saved to the database")
         return response_object
 
 @app.route('/edit/<id>', methods = ["GET", "POST"])
 def url_edit(id):
-    current_presentation = {}
-    for p in presentations:
-        if p['id'] == id:
-            current_presentation = p
-
-    response_object = {'status': 'success'}
+    pres = dbclient.presentations.find({'id': id})
+    # response_object = {'status': 'success'}
     if request.method == 'GET':
-        return jsonify(current_presentation)
+        return jsonify(pres)
     else:
         img = request.files['image']
         img_string = str(base64.b64encode(img.read()))[2:-1]
         print(type(img_string))
-        current_presentation['slides'].append(img_string)
-        return jsonify(current_presentation)
+        pres['slides'].append(img_string)
+        dbclient.presentations.insert_one(pres)
+        print("Presentation is updated and saved to the database")
+        return jsonify(pres)
 
 if __name__ == "__main__":
-    new_id = ''
+    cred = ""
+    with open("credentials.txt") as f:
+        cred = f.read()
+    try:
+        dbclient = MongoClient("mongodb://" + cred)
+        print("Successfully connected to the database")
+    except Exception:
+        print("Cannot connect to the database")
     app.run(host='0.0.0.0', debug = True)
